7fbe55461fe3b4bac36ee2d1042e0dd492b6f15e
ElenaPchelkina <56049246+ElenaPchelkina@users.noreply.github.com>
2021-08-01 16:48:11 +0300

    my commit

--- src/test/java/guru/qa/ZipFileTest.java
+++ src/test/java/guru/qa/ZipFileTest.java
@@ -2,43 +2,40 @@ package guru.qa;
 
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 
 public class ZipFileTest {
 
     @Test
-    static void zipTest() throws IOException, ZipException {
+    void zipTest() throws IOException, ZipException {
         String zipFilePath = ".src/test/resources/zip_test.zip";
         String unzipFolderPath = ".src/test/resources/unzip";
-        String zipPassword = "12345678";
+        String zipPassword = "";
         String unzipTxtFilePath = ".src/test/resources/unzip/txt_test.txt";
-        String expectedData = "Иванов Иван Иванович";
-    
-        ZipFile zipFile = new ZipFile(zipFilePath);
 
+        ZipFile zipFile = new ZipFile(zipFilePath);
         if (zipFile.isEncrypted()) {
             zipFile.setPassword(zipPassword);
         }
         zipFile.extractAll(unzipFolderPath);
-        
-        String actualData = readTextFromPath(unzipTxtFilePath);
-        
-        assertThat(actualData, containsString(expectedData));
-        
-
-
-
-
-
+        File unzippedFile = new File(unzipTxtFilePath);
+        String fileContent = FileUtils.readFileToString(unzippedFile, StandardCharsets.UTF_8);
+        assertThat(fileContent).contains("Иванов Иван Иванович");
+    }
 }
 
-    private static Object containsString(String expectedData) {
-    }
 
-    private static String readTextFromPath(String unzipTxtFilePath) {
-    }
+        
+
+
+
+
+
 
